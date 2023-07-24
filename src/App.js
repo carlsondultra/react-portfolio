6d59d81c25c9d11c24126a0f1de1077143c0d607
carlsondultra <carlsondultra@users.noreply.github.com>
2023-07-24 18:53:50 -0400

    touching up navbar and adding dotgroup for visuals

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import Navbar from "./scenes/Navbar";
-import { useState } from "react";
+import DotGroup from "./scenes/DotGroup";
+import { useEffect, useState } from "react";
 import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState('home');
+  const [isTopofPage, setIsTopOfPage] = useState(true);
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)"); //checking if current browser size minw-dith is above or below 1060px
+
+  useEffect(() =>{
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0 ) setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return <div className="app bg-deep-blue">
     <Navbar
+      isTopofPage={isTopofPage}
       selectedPage={selectedPage}
       setSelectedPage={setSelectedPage}
     />
+    <div className="w-5/6 mx-auto md:h-full"> 
+      {isAboveMediumScreens && (
+        <DotGroup 
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
+      )}
+    </div>
   </div>
 }
 
