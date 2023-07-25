3eee913153129e3140972866ed7b0d5c7c758250
carlsondultra <carlsondultra@users.noreply.github.com>
2023-07-24 20:24:15 -0400

    adding main section to landing page

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -23,9 +23,44 @@ const Landing = ({setSelectedPage}) => {
                         />
                     </div>
                 ) : (
-                    <div></div>
+                    <img
+                            alt="profile"
+                            className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full
+                                max-w-[400px] md:max-w-[600px]"
+                            src="assets/profile-image.png"
+                        />
                 )}
             </div>
+
+                {/* MAIN SECTION */}
+                <div className="z-30 basis-2/5 mt-12 md:mt-32">
+                    {/* HEADINGS */}
+                    <motion.div
+                        initial="hidden"
+                        whileInView="visible" //when div is shown on screen, run animation
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, x: -50 },
+                            visible: { opacity: 1, x: 0 }
+                        }}    
+                    >
+                        <p className="text-6xl font-playfair z-10 text-center md:text-start">
+                            Carlson {""}
+                            <span 
+                                className="xs:relative xs:text-deep-blue xs:font-semibold z-20
+                                xs:before:content-brush before:absolute before:-left-[25px]
+                                before:-top-[70px] before:z-[-1]"
+                            >
+                                Dultra
+                            </span>
+                        </p>
+
+                        <p className="mt-10 mb-7 text-sm text-center md:text-start">
+                            Welcome to my portfolio website!
+                        </p>
+                    </motion.div>
+                </div>
         </section>
     )
 }
