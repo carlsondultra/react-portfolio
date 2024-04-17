f161584022e661fded25a321e49cb2fd4b320e50
carlsondultra <carlsondultra@users.noreply.github.com>
2024-04-16 21:49:10 -0400

    adding new projects and removing old projects from section

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -34,6 +34,83 @@ const Projects = () => {
             {/* First Project */}
             <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
                 <div class="px-7">
+                    <p className="font-playfair font-semibold text-4xl"> collabd </p>
+                    <br />
+                    <p class="text-stone-400">React.js, Next.js, TailwindCSS, Convex</p>
+                    <br />
+                    <div class="container mx-auto">
+                        <p 
+                            class="text-stone-200">
+                                Collabd is a virtual workspace application that allows for real-time collaboration between users.
+                                By utilizing Clerk, Convex and Liveblocks.io, users can invite others to their virtual whiteboard
+                                and design their next big idea.  
+                            </p>
+                        <br />
+                    </div>
+                    <div class="inline-flex">
+                        <a target="_blank" rel="noreferrer" href="https://collabd.vercel.app/">
+                            <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l rounded-r">
+                                Visit Demo
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                <div>
+                    <img class="object-scale-down" src="../assets/collabd.png" alt="Collabd Screenshot"/>                
+                </div>
+            </div>
+            <br/>
+
+            {/* Line Divider */}
+            <div className="flex justify-center mt-5">
+                        <LineGradient width="w-1/2"/>
+            </div>
+            <br />
+
+            {/* Second Project */}
+            <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+                <div>
+                    <img class="object-scale-down" src="../assets/productivityplus.png" alt="Productivity Screenshot"/>                
+                </div>
+                <div class="px-7">
+                    <p className="font-playfair font-semibold text-4xl"> <span className="text-red">Productivity</span> Plus </p>
+                    <br />
+                    <p class="text-stone-400">TypeScript, Next.js, Prisma, Stripe, Tailwind, Zustand, and MySQL</p>
+                    <br />
+                    <div class="container mx-auto">
+                        <p 
+                            class="text-stone-200">
+                                Productivity Plus is a full-stack project management application that allows for increased productivity.
+                                Boards are created to get a clear view of who's doing what and what needs to get done. 
+                            </p>
+                        <br />
+                    </div>
+                    <div class="inline-flex">
+                        <a target="_blank" rel="noreferrer" href="https://github.com/carlsondultra/productivityplus">
+                            <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">
+                                View Repository
+                            </button>
+                        </a>
+                        <a target="_blank" rel="noreferrer" href="https://productivityplus.vercel.app/">
+                            <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r">
+                                Visit Demo
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <br/>
+
+            {/* Line Divider */}
+            <div className="flex justify-center mt-5">
+                        <LineGradient width="w-1/2"/>
+            </div>
+            <br />
+
+            {/* Third Project */}
+
+            <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+                <div class="px-7">
                     <p className="font-playfair font-semibold text-4xl"> Carlson's <span className="text-red">Convenience</span> </p>
                     <br />
                     <p class="text-stone-400">NextJS, TailwindCSS, Zustand, Stripe</p>
@@ -71,7 +148,9 @@ const Projects = () => {
             </div>
             <br />
 
-            {/* Second Project */}
+            
+            {/* Fourth Project */}
+
             <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
                 <div>
                     <img class="object-scale-down" src="../assets/quizproject.jpeg" alt="Quiz Screenshot"/>                
@@ -110,25 +189,6 @@ const Projects = () => {
             </div>
             <br />
 
-            {/* Third Project */}
-           
-
-            {/* Line Divider */}
-            <div className="flex justify-center mt-5">
-                        <LineGradient width="w-1/2"/>
-            </div>
-            <br />
-
-            
-            {/* Fourth Project */}
-            
-
-            {/* Line Divider */}
-            <div className="flex justify-center mt-5">
-                        <LineGradient width="w-1/2"/>
-            </div>
-            <br />
-
             
             {/* Fifth Project */}
             <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
@@ -146,13 +206,10 @@ const Projects = () => {
                     </div>
                     <div class="inline-flex">
                         <a target="_blank" rel="noreferrer" href="https://github.com/carlsondultra/review">
-                            <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">
+                            <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l rounded-r">
                                 View Repository
                             </button>
                         </a>
-                            <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 opacity-50 cursor-not-allowed rounded-r">
-                                Demo Not Available
-                            </button>
                     </div>
                 </div>
                 <div>
